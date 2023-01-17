960433dcda46bcaca8885178341be8dc78f79530
Ricardo Weir <ricardo.weir@suse.com>
2023-01-16 18:26:53 -0700

    kubebuilder create API
    Create CRD and controller.

---
+++ api/v1/subjectregistrar_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SubjectRegistrarSpec defines the desired state of SubjectRegistrar
+type SubjectRegistrarSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of SubjectRegistrar. Edit subjectregistrar_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SubjectRegistrarStatus defines the observed state of SubjectRegistrar
+type SubjectRegistrarStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// SubjectRegistrar is the Schema for the subjectregistrars API
+type SubjectRegistrar struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SubjectRegistrarSpec   `json:"spec,omitempty"`
+	Status SubjectRegistrarStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SubjectRegistrarList contains a list of SubjectRegistrar
+type SubjectRegistrarList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SubjectRegistrar `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SubjectRegistrar{}, &SubjectRegistrarList{})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	rbacv1 "cattle.io/role-keeper/api/v1"
+	"cattle.io/role-keeper/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(rbacv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -85,6 +89,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.SubjectRegistrarReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SubjectRegistrar")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
